4f2c4e3374ab7a340c42e4af828b35ba36b91767
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-02-27 14:40:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link href="//db.onlinewebfonts.com/c/376af66aec8d4ff2f765722f20d28baf?family=X-Files" rel="stylesheet"
+    type="text/css" />
+
+  <link rel="stylesheet" href="style.css">
+
+
+  <title>Generateur de citations</title>
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row">
+      <div class="main">
+        <div class="col-md-6 col-sm-12 col-xs-12" id="content">
+          <h1>Generateur de citations <br> The X-Files</h1>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="quote">
+
+    </div>
+    <center> <button type="button" class="btn btn-dark float-center" id="button">Pour generer une citation<br>Cliquez
+        ici! </button>
+  </div>
+  <br>
+
+  </div>
+
+
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+
+</html>
